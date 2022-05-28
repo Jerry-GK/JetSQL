0d47b8ea551d8a190f1d59e0af98d93857dcea7d
Jerry-GK <364250867@qq.com>
2022-05-28 23:23:47 +0800

    fix a small bug

--- src/catalog/catalog.cpp
+++ src/catalog/catalog.cpp
@@ -62,7 +62,8 @@ CatalogManager::CatalogManager(BufferPoolManager *buffer_pool_manager, LockManag
     catalog_meta_ = CatalogMeta::NewInstance(heap_);
   } else {
     p = buffer_pool_manager->FetchPage(CATALOG_META_PAGE_ID);
-    throw -1;
+    if(p==nullptr)
+      throw -1;
     //ASSERT(p != nullptr, "No catalog metapage for the existed database!");
     catalog_meta_ = CatalogMeta::DeserializeFrom(p->GetData(), heap_);
   }
